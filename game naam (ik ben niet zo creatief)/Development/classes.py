6b4872a9f6401426b291aca740e13b54aae6f216
AidanMiddel <70641287+AidanMiddel@users.noreply.github.com>
2020-12-08 16:15:35 +0100

    en opdracht 1

--- game naam (ik ben niet zo creatief)/Development/classes.py
+++ game naam (ik ben niet zo creatief)/Development/classes.py
@@ -1,32 +1,19 @@
-class Plane :
+class Mario :
 
-    _lives = 1
+    _lives = 3
     _speed = 30.0
-    _bomber = 1
-    _ammunition = 5
 
-    def __init__(self, levens, snelheid, bommer, munitie):
-        self.levens = levens
-        self.snelheid = snelheid
-        self.bommer = bommer
-        self.munitie = munitie
-
-    def planelives(leef):
-        print("Plane's lives is: " , leef.planelives)
-    
-    def planespeed(snel):
-        print("Plane's speed is: " , snel.planespeed)
-
-    def planebommer(bom):
-        print("Plane has bommer: ", bom.planebommer)
-
-    def planeammunition(kogels):
-        print("Plane has" , kogels.planeammunition , "boms in storage")
+    def Test(self):
+        print("Hallo")
+        print("Speed is: ", self._speed)
 
 
-littleplane = Plane(1, 30, 1, 5)
-littleplane.planelives()
-littleplane.planespeed()
-littleplane.planebommer()
-littleplane.planeammunition()
+instanceMario = Mario()
+nogEenMario = Mario()
 
+print( instanceMario._lives )
+instanceMario.Test()
+instanceMario._speed = 50.5
+
+print("instanceMario snelheid:", instanceMario._speed)
+print("nogEenMario snelheid:", nogEenMario._speed) 
